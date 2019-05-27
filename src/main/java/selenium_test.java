1a70276bdfe3ec50f61433ba35b514e2a3a9c8a9
tjurrw <clrnr88@gmail.com>
2019-05-27 14:47:46 +0900

    15th commit

--- src/main/java/selenium_test.java
+++ src/main/java/selenium_test.java
@@ -24,3 +24,33 @@ public class selenium_test {
         }
     }
 }
+
+/*
+
+        driver.get("http://admin:admin@192.168.2.180");
+        Thread.sleep(5000);
+
+        WebElement changeSsid = driver.findElement(By.name("ssid0"));
+        changeSsid.clear();
+        changeSsid.sendKeys(a[].ssid);
+
+        WebElement getPw = driver.findElement(By.name("pskValue0_show"));
+        getPw.click(); //비밀번호 획득
+
+        WebElement adminId = driver.findElement(By.name("username"));
+        adminId.clear();
+        adminId.sendKeys("admin");
+        WebElement adminPw = driver.findElement(By.name("newpass"));
+        adminPw.clear();
+        adminPw.sendKeys("pwd");
+        WebElement adminCpw = driver.findElement(By.name("confpass"));
+        adminCpw.clear();
+        adminCpw.sendKeys("pwd");
+        WebElement changeIp = driver.findElement(By.name("lan_ip"));
+        changeIp.click();
+
+        Thread.sleep(2000);
+        WebElement passWord = driver.findElement(By.name("user_pw"));
+        passWord.sendKeys("test");
+
+ */
